1ee8f8de7996d0bcbc5d660857c35b7523fff592
Andrei Atasiei <andrei.atasiei@mambu.com>
2015-02-19 11:00:30 +0200

    MBU-7747 Create a Chinese implementation for the numbers2words library added missing javadoc

--- src/java/com/mambu/number2words/internal/simplifiedchinese/visitors/SimplifiedChineseTokenVisitor.java
+++ src/java/com/mambu/number2words/internal/simplifiedchinese/visitors/SimplifiedChineseTokenVisitor.java
@@ -22,12 +22,24 @@ import com.mambu.number2words.parsing.visitors.AbstractTranscribingVisitor;
 public class SimplifiedChineseTokenVisitor extends AbstractTranscribingVisitor {
 
 	/**
+	 * {@link NullValueToken}s may need to be printed. When it gets printed this is the value used.
+	 */
+	private static final long NULL_TOKEN_VALUE = 0L;
+
+	/**
 	 * There is no word separator for Simplified Chinese.
 	 */
 	private static final String WORD_SEPARATOR = "";
 
+	/**
+	 * Flag that will be true until after the first printable token is visited.
+	 */
 	private boolean isFirstPrintableToken = true;
 
+	/**
+	 * Flag that tells whether the last printed token was a {@link NullValueToken}. If that is <code>true</code> then
+	 * the next {@link NullValueToken} must be ignored.
+	 */
 	private boolean lastPrintWasNull = false;
 
 	/**
@@ -40,24 +52,41 @@ public class SimplifiedChineseTokenVisitor extends AbstractTranscribingVisitor {
 	 *            - the context that holds the number to word mapping information.
 	 * 
 	 */
-	public SimplifiedChineseTokenVisitor(StringBuilder builder, TranscriptionContext context) {
+	public SimplifiedChineseTokenVisitor(final StringBuilder builder, final TranscriptionContext context) {
 		super(context, builder, WORD_SEPARATOR);
 	}
 
+	/**
+	 * {@inheritDoc}
+	 * <p>
+	 * Null tokens represents empty groups and in Simplified Chinese empty groups need to be marked when they appear
+	 * before other non-zero values.
+	 */
 	@Override
-	public Void visitNullValue(NullValueToken token) {
+	public Void visitNullValue(final NullValueToken token) {
+
+		// don't print null tokens when their the first printable token found
+		// or when the previous printed token was a null
 		if (!isFirstPrintableToken && !lastPrintWasNull) {
-			lastPrintWasNull = true;
+
 			isFirstPrintableToken = false;
-			builder.append(context.asWord(0L, GrammaticalNumber.SINGULAR, WordForm.DEFAULT));
+			lastPrintWasNull = true;
+
+			builder.append(context.asWord(NULL_TOKEN_VALUE, GrammaticalNumber.SINGULAR, WordForm.DEFAULT));
 		}
+
 		return null;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
-	public Void visitMappedValue(MappedValueToken token) {
+	public Void visitMappedValue(final MappedValueToken token) {
+
 		isFirstPrintableToken = false;
 		lastPrintWasNull = false;
+
 		return super.visitMappedValue(token);
 	}
 
